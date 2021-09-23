cf21f5082ba8281082af67b4eb5213201542d467
Arun <ArunachalamB>
2021-09-23 15:28:24 +0530

    added entities

--- src/app/Shopping.Promo.Core/Infra/Class1.cs
+++ src/app/Shopping.Promo.Core/Infra/Class1.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ShoppingPromoCore.Entities;
+using ShoppingPromoCore.Interfaces;
 
 namespace ShoppingPromoCore.Infra
 {
-    class Class1
+    public class PromotionRuleRepository : IPromotionRuleRepo
     {
+        public List<PromotionDetails> GetPromotionRulesBySkuId(List<long> ids)
+        {
+            return new List<PromotionDetails>();
+        }
+    }
+    public class SKURepository : ISKURepository
+    {
+        public decimal GetSKUPrice(long skuid)
+        {
+            return 1;
+        }
     }
 }
--- src/app/Shopping.Promo.Core/Interfaces/Class1.cs
+++ src/app/Shopping.Promo.Core/Interfaces/Class1.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ShoppingPromoCore.Entities;
 
 namespace ShoppingPromoCore.Interfaces
 {
-    class Class1
+    public interface IPromotionRuleRepo
     {
+        List<PromotionDetails> GetPromotionRulesBySkuId(List<long> ids);
+    }
+    public interface ISKURepository
+    {
+        decimal GetSKUPrice(long skuid);
     }
 }
